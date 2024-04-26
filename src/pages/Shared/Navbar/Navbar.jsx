6d3eebf22f12babdc87ee7cd49bb6a47d40e6cbf
the-user01 <sakibcom02@gmail.com>
2024-04-27 01:34:13 +0600

    updated scr & navbar file

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
 import demoImg from '../../../assets/user.png';
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
+
 
 const Navbar = () => {
 
@@ -10,15 +13,15 @@ const Navbar = () => {
     );
 
     const handleChange = e => {
-        if(e.target.checked){
+        if (e.target.checked) {
             setTheme("dark");
         }
-        else{
+        else {
             setTheme("light");
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         localStorage.setItem("theme", theme);
         const localTheme = localStorage.getItem("theme");
         document.querySelector('html').setAttribute("data-theme", localTheme);
@@ -62,11 +65,11 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-            <div className="mr-6 mt-2">
+                <div className="mr-6 mt-2">
                     <label className="swap swap-rotate">
 
                         {/* this hidden checkbox controls the state */}
-                        <input type="checkbox" onChange={handleChange} checked = {theme === "light" ? false : true} className="theme-controller" value="synthwave" />
+                        <input type="checkbox" onChange={handleChange} checked={theme === "light" ? false : true} className="theme-controller" value="synthwave" />
 
                         {/* sun icon */}
                         <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
@@ -83,14 +86,16 @@ const Navbar = () => {
                             {
                                 loader ? <span className="loading loading-dots loading-sm"></span> :
                                     <>
-                                        <div className="tooltip tooltip-bottom mr-4" data-tip={user.displayName}>
-                                            <div className="avatar">
-                                                <div className="w-12 rounded-full">
-                                                    <img src={user.photoURL} />
-                                                </div>
-                                            </div>
+                                        <div className="mr-4">
+                                            <a
+                                                data-tooltip-id="my-tooltip"
+                                                data-tooltip-content={user.displayName}
+                                                data-tooltip-place="top"
+                                            >
+                                                <img src={user.photoURL} className="w-12 h-12 rounded-full" />
+                                            </a>
+                                            <Tooltip id="my-tooltip" />
                                         </div>
-
                                         <button className="btn btn-primary" onClick={handleLogOut}>SignOut</button>
                                     </>
                             }
