2416b788162e083ddf94e4c9d85d874d791dbb47
the-user01 <sakibcom02@gmail.com>
2024-04-30 23:49:53 +0600

    update codes

--- src/pages/Main/HomePages/Banner.jsx
+++ src/pages/Main/HomePages/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div>
 
-            <div className="carousel w-full md:h-[450px] mt-6">
+            <div className="carousel w-full md:h-[450px] mt-10">
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src="https://i.ibb.co/kxhRbbJ/premium-photo-1676393904171-738ff11eceaa.jpg" className="w-full " />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
